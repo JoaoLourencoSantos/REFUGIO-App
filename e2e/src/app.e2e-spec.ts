c14baf130794d1d911e1fcbd923dc03b0e08b8a2
João marcos <lourencosjm41@gmail.com>
2020-09-09 01:19:03 -0300

    ajuste

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('construo-backoffice app is running!');
+    expect(page.getTitleText()).toEqual('project-tis app is running!');
   });
 
   afterEach(async () => {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 // PRODUCTION ENVIROMENT
 export const environment = {
   production: true,
-  API_BASEPATH: 'https://backoffice.api-construoapp.com.br/api',
 };
